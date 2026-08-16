b5722ba06d5899991445e5ac7be6922b34f76a06
Jasper-Spittaels <hamytje9@gmail.com>
2026-08-17 01:57:20 +0200

    Deploy GrapesJS Editor

--- InstallDependencies.bat
+++ InstallDependencies.bat
@@ -1,4 +1,3 @@
 @echo off
 call npm install grapesjs grapick toastr jquery grapesjs-preset-webpage grapesjs-blocks-basic grapesjs-plugin-forms grapesjs-component-countdown grapesjs-plugin-export grapesjs-tabs grapesjs-custom-code grapesjs-touch grapesjs-parser-postcss grapesjs-tooltip grapesjs-tui-image-editor grapesjs-typed grapesjs-style-bg
-call npm install fabric
 pause
